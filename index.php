908e48990b583d5e928d1211c2e455496de818e2
BrandonPortillo <64477890+BrandonPortillo@users.noreply.github.com>
2020-04-28 08:21:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<link rel="stylesheet" href="estilo1.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+</head>
 <body>
 <?php
 echo "Datos Personales";
@@ -13,3 +19,4 @@ echo $_POST["password"];
 echo "<br>";
 ?>
 </body>
+</html>
