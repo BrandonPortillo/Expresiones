dbde71667a84236d332ec23b89b5b88067ddd34b
BrandonPortillo <64477890+BrandonPortillo@users.noreply.github.com>
2020-04-28 08:04:55 -0500

    Add files via upload

---
+++ ver.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="estilo1.css">
+<body>
+<?php
+echo "Datos Personales";
+echo "<br>";
+echo $_POST["nombre"];
+echo "<br>";
+echo $_POST["apellidos"];
+echo "<br>";
+echo $_POST["email"];
+echo "<br>";
+echo $_POST["password"];
+echo "<br>";
+?>
+</body>
